7c40baedfa77cc91b52dde8cf245e9adbf88d768
andytrigg <andy@sloshydog.com>
2014-12-08 08:40:18 +1100

    added square functions to MyList module

--- lists/mylist1.exs
+++ lists/mylist1.exs
@@ -1,4 +1,6 @@
 defmodule MyList do
   def len([]), do: 0
   def len([_head | tail]), do: 1 + len(tail)
+  def square([]), do: []
+  def square([ head | tail ]), do: [ head*head | square(tail) ] 
 end
