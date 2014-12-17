91d0645881582702770250386004f337090b3091
andytrigg <andy@sloshydog.com>
2014-12-18 10:56:01 +1100

    dynamic nested examples

---
+++ maps/dynamic_nested.exs
@@ -0,0 +1,24 @@
+nested = %{
+  buttercup: %{
+    actor: %{
+      first: "Robin"
+      last: "Wright"
+    },
+    role: "princess"
+  },
+  westley: %{
+    actor: %{
+      first: "Carey",
+      last: "Ewes"
+    },
+    role: "farm boy"
+  }
+}
+
+IO.inspect get_in(nested, [:buttercup])
+
+IO.inspect get_in(nested, [:buttercup, :actor])
+
+IO.inspect get_in(nested, [:buttercup, :actor, :first])
+
+IO.inspect put_in(nested, [:westley, :actor, :last], "Elwes")
