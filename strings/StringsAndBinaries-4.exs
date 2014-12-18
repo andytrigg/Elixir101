e4d88c2b04031d454dab9ebf0437f43a14543525
Andy Trigg <atrigg@aconex.com>
2014-12-18 15:46:19 +1100

    some string examples

---
+++ strings/StringsAndBinaries-4.exs
@@ -0,0 +1,33 @@
+defmodule Parse do
+  
+  def calculate(expression) do
+    { n1, rest } = parse_number(expression)
+    rest         = skip_spaces(rest)
+    { op, rest } = parse_operator(rest)
+    rest         = skip_spaces(rest)
+    { n2, [] }   = parse_number(rest)
+    op.(n1, n2)
+  end
+
+  defp parse_number(expression), do: _parse_number({ 0, expression })
+
+  defp _parse_number({value, [ digit | rest ] }) when digit in ?0..?9 do
+    _parse_number({ value*10 + digit - ?0, rest})
+  end
+
+  defp _parse_number(result), do: result
+
+  defp skip_spaces([ ?  | rest ]), do: skip_spaces(rest)
+  defp skip_spaces(rest),          do: rest
+
+  defp parse_operator([ ?+ | rest ]), do: { &1+&2, rest }
+  defp parse_operator([ ?- | rest ]), do: { &1-&2, rest }
+  defp parse_operator([ ?* | rest ]), do: { &1*&2, rest }
+  defp parse_operator([ ?/ | rest ]), do: { div(&1, &2), rest }
+
+end
+
+IO.inspect Parse.calculate('23+45')     #=> 68
+IO.inspect Parse.calculate('34  - 56')  #=> -22
+IO.inspect Parse.calculate('12 * 23')   #=> 276
+IO.inspect Parse.calculate('123 / 8')   #=> 15
